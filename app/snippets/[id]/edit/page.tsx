937b54fe9c30bce669f9d94e5174a9dc5525b3e1
KwabSA <141826847+KwabSA@users.noreply.github.com>
2024-12-30 21:11:49 +0000

    Fetched record for editing

--- app/snippets/[id]/edit/page.tsx
+++ app/snippets/[id]/edit/page.tsx
@@ -1,13 +1,23 @@
+import { notFound } from "next/navigation";
+import { db } from "@/db";
+
 interface EditSnippetProps {
   params: {
     id: string;
   };
 }
 
-const EditSnippetPage = (props: EditSnippetProps) => {
+const EditSnippetPage = async (props: EditSnippetProps) => {
   const id = parseInt(props.params.id);
+  const snippet = await db.snippet.findFirst({
+    where: { id },
+  });
 
-  return <div className="text-gray-300">Editing {id}</div>;
+  if (!snippet) {
+    return notFound();
+  }
+
+  return <div className="text-gray-300 m-4">Editing {snippet?.title}</div>;
 };
 
 export default EditSnippetPage;
