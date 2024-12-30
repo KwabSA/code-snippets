55d3e267835add3919d8a42f6095af0258e35eed
KwabSA <141826847+KwabSA@users.noreply.github.com>
2024-12-30 01:09:01 +0000

    List records on Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,14 @@
-const Home = () => {
-  return <div>Home </div>;
+import { db } from "@/db";
+
+const Home = async () => {
+  // retrieve records
+  const snippets = await db.snippet.findMany();
+
+  // list records
+  const listSnippets = snippets.map((snippet) => {
+    return <div key={snippet.id}>{snippet.title}</div>;
+  });
+  return <div className="text-gray-300 mt-8">{listSnippets} </div>;
 };
 
 export default Home;
