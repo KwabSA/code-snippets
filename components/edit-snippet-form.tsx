4d3afe8613f16838432942802192b08f356d6ed5
KwabSA <141826847+KwabSA@users.noreply.github.com>
2025-01-03 06:25:48 +0000

    Server action to save edited snippet in client component

--- components/edit-snippet-form.tsx
+++ components/edit-snippet-form.tsx
@@ -3,6 +3,7 @@
 import type { Snippet } from "@prisma/client";
 import Editor from "@monaco-editor/react";
 import { useState } from "react";
+import * as actions from "@/actions";
 
 interface EditSnippetFormProps {
   snippet: Snippet;
@@ -15,6 +16,8 @@ const EditSnippetForm = ({ snippet }: EditSnippetFormProps) => {
     console.log(code);
     setCode(value);
   };
+  // calling server action
+  const editSnippetAction = actions.editSnippet.bind(null, snippet.id, code);
 
   return (
     // <div className="text-gray-300 m-4 font-bold">Editing: {snippet.title}</div>
@@ -27,6 +30,15 @@ const EditSnippetForm = ({ snippet }: EditSnippetFormProps) => {
         options={{ minimap: { enabled: false } }}
         onChange={handleEditorChange}
       />
+
+      <form action={editSnippetAction} className="flex justify-end p-2">
+        <button
+          type="submit"
+          className="py-2 px-4 border rounded hover:bg-blue-500"
+        >
+          Save
+        </button>
+      </form>
     </div>
   );
 };
